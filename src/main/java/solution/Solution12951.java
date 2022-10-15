ca3c85f2aa7e58b1f3c7551c25308ccaff771046
taetanee <xpxksl@gmail.com>
2022-10-16 05:25:07 +0900

    코테 12951 재시도

--- src/main/java/solution/Solution12951.java
+++ src/main/java/solution/Solution12951.java
@@ -5,32 +5,64 @@ JadeonCase 문자열 만들기
 https://programmers.co.kr/learn/courses/30/lessons/12951?language=java
  */
 
+import common.CommonUtil;
+
 public class Solution12951 {
-    public static void main(String[] args) {
-        Solution12951 s1 = new Solution12951();
-        s1.solution("3people unFollowed me");
-    }
+	public static void main(String[] args) {
+		Solution12951 s1 = new Solution12951();
+        System.out.println(s1.solution("3people unFollowed me"));
 
-    public String solution(String s) {
-        String answer = "";
+		Solution12951 s2 = new Solution12951();
+        System.out.println(s2.solution("For The Last Week"));
+	}
 
-        boolean nextChar = true;
-        for( char c : s.toCharArray()) {
-            if(nextChar == true && c >= 'a' && c <= 'z'){
-                answer += (char) (c - 32);
-            } else if(nextChar == false && c >= 'A' && c <= 'Z'){
-                answer += (char) (c + 32);
+//	public String solution(String s) {
+//		String answer = "";
+//
+//		boolean nextChar = true;
+//		for( char c : s.toCharArray()) {
+//			if(nextChar == true && c >= 'a' && c <= 'z'){
+//				answer += (char) (c - 32);
+//			} else if(nextChar == false && c >= 'A' && c <= 'Z'){
+//				answer += (char) (c + 32);
+//			} else {
+//				answer += c;
+//			}
+//
+//			if(c == ' '){
+//				nextChar = true;
+//			} else {
+//				nextChar = false;
+//			}
+//
+//		}
+//		return answer;
+//	}
+
+	public String solution(String s) {
+		String answer = "";
+
+		for (int i = 0; i < s.length(); i++) {
+		    boolean upperFlag = false;
+		    char c = s.charAt(i);
+
+		    if( i == 0 ){
+                upperFlag = true;
+            } else if( i >= 1 && s.charAt(i-1) == ' '){
+                upperFlag = true;
+            } else if( i >= 1 && s.charAt(i-1) != ' '){
+                upperFlag = false;
             } else {
-                answer += c;
+                upperFlag = false;
             }
 
-            if(c == ' '){
-                nextChar = true;
+		    if( upperFlag == true ){
+                answer += Character.toUpperCase(c);
             } else {
-                nextChar = false;
+                answer += Character.toLowerCase(c);
             }
+		}
 
-        }
-        return answer;
-    }
+		return answer;
+	}
 }
