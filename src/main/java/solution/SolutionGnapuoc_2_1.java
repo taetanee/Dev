acefbbac9dfe7b09b4116cb586a4939ad42c8cdc
taetanee <xpxksl@gmail.com>
2022-10-18 14:11:39 +0900

    Update SolutionGnapuoc_2_1.java

--- src/main/java/solution/SolutionGnapuoc_2_1.java
+++ src/main/java/solution/SolutionGnapuoc_2_1.java
@@ -1,36 +1,60 @@
 package solution;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class SolutionGnapuoc_2_1 {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		SolutionGnapuoc_2_1 s1 = new SolutionGnapuoc_2_1();
-		System.out.println(s1.solution(new int[]{5, 3, 9, 13}, 8));
-		//System.out.println(s1.solution(new int[]{5,3,9,13},7));
-	}
+        SolutionGnapuoc_2_1 s1 = new SolutionGnapuoc_2_1();
+        System.out.println(s1.solution(new int[]{5, 3, 9, 13}, 8)); // true
+        System.out.println(s1.solution(new int[]{5, 3, 9, 13}, 7)); // false
 
-	public boolean solution(int[] arr, int n) {
-		boolean answer = false;
+        int[] arr = new int[500000];
+        for(int i=0;i<500000;i++) {
+            arr[i] = i+1;
+        }
+        System.out.println(s1.solution(arr,999999)); //true
+        System.out.println(s1.solution(arr,1000001)); //false
+    }
 
-		int length = arr.length;
+    public boolean solution(int[] arr, int n) {
+        HashMap<Integer,Boolean> hashMap = new HashMap();
+        for(int i=0;i<arr.length;i++){
+            hashMap.put(arr[i], true);
+        }
+        for(Integer x : hashMap.keySet()){
+            if( hashMap.get(n-x) != null){
+                return true;
+            }
+        }
+        return false;
+    }
 
-		for (int i = 0; i < length; i++) {
-
-			if (arr[i] > n) {
-				continue;
-			}
-
-			for (int j = i + 1; j < length; j++) {
-				if (arr[j] > n) {
-					continue;
-				}
-
-				if (arr[i] + arr[j] == n) {
-					answer = true;
-					return answer;
-				}
-			}
-		}
-
-		return answer;
-	}
+//    public boolean solution2(int[] arr, int n) {
+//        boolean answer = false;
+//
+//        int length = arr.length;
+//
+//        for (int i = 0; i < length; i++) {
+//
+//            if (arr[i] > n) {
+//                continue;
+//            }
+//
+//            for (int j = i + 1; j < length; j++) {
+//                if (arr[j] > n) {
+//                    continue;
+//                }
+//
+//                if (arr[i] + arr[j] == n) {
+//                    answer = true;
+//                    return answer;
+//                }
+//            }
+//        }
+//
+//        return answer;
+//    }
 }
