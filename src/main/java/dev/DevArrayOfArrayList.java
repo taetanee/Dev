d7290d174de5e2592196e49e647d440b67451e9f
taetanee <xpxksl@gmail.com>
2022-12-15 06:49:20 +0900

    공부용 DevArrayOfArrayList 추가

---
+++ src/main/java/dev/DevArrayOfArrayList.java
@@ -0,0 +1,14 @@
+package dev;
+
+import java.util.ArrayList;
+
+public class DevArrayOfArrayList {
+
+	public static void main(String[] args) {
+		ArrayList<String[]> arrayList1 = new ArrayList<>();
+		arrayList1.add(new String[2]);
+
+		ArrayList<Integer[]> arrayList2 = new ArrayList<>();
+		arrayList2.add(new Integer[2]);
+	}
+}
