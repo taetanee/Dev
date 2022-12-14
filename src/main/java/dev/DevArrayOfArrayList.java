04020209511a7b436d2d7c905b55cfc4b9218b72
taetanee <xpxksl@gmail.com>
2022-12-15 06:58:07 +0900

    공부용 DevArrayOfArrayList 추가

--- src/main/java/dev/DevArrayOfArrayList.java
+++ src/main/java/dev/DevArrayOfArrayList.java
@@ -8,12 +8,12 @@ public class DevArrayOfArrayList {
 		ArrayList<String[]> arrayList1 = new ArrayList<>();
 		arrayList1.add(new String[2]);
 
+
 		ArrayList<Integer[]> arrayList2 = new ArrayList<>();
 		arrayList2.add(new Integer[2]);
 
 
 		ArrayList<Integer[][]> arrayList3 = new ArrayList<>();
-
 		Integer[][] temp = new Integer[1][2];
 		temp[0][0] = 1;
 		temp[0][1] = 2;
