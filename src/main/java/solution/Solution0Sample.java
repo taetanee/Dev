7eccb7cf3ff536bafe0c07a890cf6fa411563184
taetanee <xpxksl@gmail.com>
2022-12-15 05:53:57 +0900

    Solution0Sample 개선 (e.printStackTrace)

--- src/main/java/solution/Solution0Sample.java
+++ src/main/java/solution/Solution0Sample.java
@@ -5,14 +5,24 @@ package solution;
 https://programmers.co.kr/learn/courses/30/lessons/xxxxx
  */
 
+import java.util.HashMap;
+
 public class Solution0Sample {
 	public static void main(String[] args) {
 
-		Solution0Sample s1 = new Solution0Sample();
-		System.out.println(s1.solution());
+		try {
+			Solution0Sample s1 = new Solution0Sample();
+			System.out.println(s1.solution());
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 
-		Solution0Sample s2 = new Solution0Sample();
-		System.out.println(s2.solution());
+		try {
+			Solution0Sample s2 = new Solution0Sample();
+			System.out.println(s2.solution());
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	public int solution() {
