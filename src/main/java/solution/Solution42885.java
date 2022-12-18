918bec5bc77d517562754a2fe9b7c9504b7723e7
taetanee <xpxksl@gmail.com>
2022-12-18 17:26:35 +0900

    Solution42885 초안

---
+++ src/main/java/solution/Solution42885.java
@@ -0,0 +1,30 @@
+package solution;
+
+/*
+구명보트
+https://school.programmers.co.kr/learn/courses/30/lessons/42885
+ */
+
+public class Solution42885 {
+	public static void main(String[] args) {
+
+		try {
+			Solution42885 s1 = new Solution42885();
+			System.out.println(s1.solution(new int[]{70, 50, 80, 50},100));//3
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+		try {
+			Solution42885 s2 = new Solution42885();
+			System.out.println(s2.solution(new int[]{70, 80, 50},100));//3
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public int solution(int[] people, int limit) {
+		int answer = 0;
+		return answer;
+	}
+}
