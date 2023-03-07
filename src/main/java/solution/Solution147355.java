a54a6cda4f68de98d6bcb3e82af155c47d9e7560
taetanee <xpxksl@gmail.com>
2023-03-08 06:48:51 +0900

    Solution147355 코테(초안만 작성함)

---
+++ src/main/java/solution/Solution147355.java
@@ -0,0 +1,37 @@
+package solution;
+
+/*
+크기가 작은 부분 문자열
+https://school.programmers.co.kr/learn/courses/30/lessons/147355
+ */
+
+public class Solution147355 {
+	public static void main(String[] args) {
+
+		try {
+			Solution147355 _s = new Solution147355();
+			System.out.println(_s.solution("3141592", "271"));//2
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+		try {
+			Solution147355 _s = new Solution147355();
+			System.out.println(_s.solution("500220839878","7"));//8
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+		try {
+			Solution147355 _s = new Solution147355();
+			System.out.println(_s.solution("10203","15"));//3
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public int solution(String t, String p) {
+		int answer = 0;
+		return answer;
+	}
+}
