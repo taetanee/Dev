8d2e38d8cddcdfcb3f4a4e1ef660b549edb72b66
taetanee <xpxksl@gmail.com>
2023-03-09 06:36:51 +0900

    Solution64065 코테

---
+++ src/main/java/solution/Solution64065.java
@@ -0,0 +1,67 @@
+package solution;
+
+/*
+튜플
+https://school.programmers.co.kr/learn/courses/30/lessons/64065
+https://velog.io/@qwe916/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-64065-%ED%8A%9C%ED%94%8C : 참고해서 풀었음
+ */
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution64065 {
+	public static void main(String[] args) {
+
+		try {
+			Solution64065 _s = new Solution64065();
+			System.out.println(_s.solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"));//[2, 1, 3, 4]
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+		try {
+			Solution64065 _s = new Solution64065();
+			System.out.println(_s.solution("{{1,2,3},{2,1},{1,2,4,3},{2}}"));//[2, 1, 3, 4]
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+		try {
+			Solution64065 _s = new Solution64065();
+			System.out.println(_s.solution("{{20,111},{111}}"));//[111, 20]
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+		try {
+			Solution64065 _s = new Solution64065();
+			System.out.println(_s.solution("{{123}}"));//[123]
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+		try {
+			Solution64065 _s = new Solution64065();
+			System.out.println(_s.solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"));//[3, 2, 4, 1]
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static int[] solution(String s) {
+		Set<String> set = new HashSet<>();
+		String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+		Arrays.sort(arr, (a, b)->{
+			return a.length() - b.length();
+		});
+		int[] answer = new int[arr.length];
+		int idx = 0;
+		for(String s1 : arr) {
+			for(String s2 : s1.split(",")) {
+				if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
+			}
+		}
+		return answer;
+	}
+}
