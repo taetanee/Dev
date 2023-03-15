c95a67ad3120b8ab18e5e2a04b43f1c0de10880d
taetanee <xpxksl@gmail.com>
2023-03-16 06:45:57 +0900

    Solution12985 코테 초안

---
+++ src/main/java/solution/Solution12985.java
@@ -0,0 +1,34 @@
+package solution;
+
+/*
+문제제에에목
+https://programmers.co.kr/learn/courses/30/lessons/xxxxx
+ */
+
+public class Solution12985 {
+	public static void main(String[] args) {
+
+		try {
+			Solution12985 _s = new Solution12985();
+			System.out.println(_s.solution(8,4,7));//3
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+//		try {
+//			Solution12985 _s = new Solution12985();
+//			System.out.println(_s.solution());
+//		} catch (Exception e){
+//			e.printStackTrace();
+//		}
+	}
+
+	public int solution(int n, int a, int b){
+		int answer = 0;
+
+		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+		System.out.println("Hello Java");
+
+		return answer;
+	}
+}
