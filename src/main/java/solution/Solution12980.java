796be50f29a867104a754687fd9ef0fbf6e3069f
taetanee <xpxksl@gmail.com>
2023-03-18 00:47:01 +0900

    코테 Solution12980 초안

---
+++ src/main/java/solution/Solution12980.java
@@ -0,0 +1,41 @@
+package solution;
+
+/*
+점프와 순간 이동
+https://school.programmers.co.kr/learn/courses/30/lessons/12980
+ */
+
+public class Solution12980 {
+	public static void main(String[] args) {
+
+		try {
+			Solution12980 _s = new Solution12980();
+			System.out.println(_s.solution(5));//2
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+		try {
+			Solution12980 _s = new Solution12980();
+			System.out.println(_s.solution(6));//2
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+		try {
+			Solution12980 _s = new Solution12980();
+			System.out.println(_s.solution(2));//5
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public int solution(int n) {
+		int ans = 0;
+
+		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+		System.out.println("Hello Java");
+
+		return ans;
+	}
+}
