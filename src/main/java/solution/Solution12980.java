9d845ada15f1cf531d26e8095c6e79ba984c1a75
taetanee <xpxksl@gmail.com>
2023-03-18 15:46:21 +0900

    코테 Solution12980

--- src/main/java/solution/Solution12980.java
+++ src/main/java/solution/Solution12980.java
@@ -3,39 +3,44 @@ package solution;
 /*
 점프와 순간 이동
 https://school.programmers.co.kr/learn/courses/30/lessons/12980
+https://jongwoon.tistory.com/157 : 참고하여 풀었음
  */
 
 public class Solution12980 {
 	public static void main(String[] args) {
 
+//		try {
+//			Solution12980 _s = new Solution12980();
+//			System.out.println(_s.solution(5));//2
+//		} catch (Exception e){
+//			e.printStackTrace();
+//		}
+//
+//		try {
+//			Solution12980 _s = new Solution12980();
+//			System.out.println(_s.solution(6));//2
+//		} catch (Exception e){
+//			e.printStackTrace();
+//		}
+//
 		try {
 			Solution12980 _s = new Solution12980();
-			System.out.println(_s.solution(5));//2
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-
-		try {
-			Solution12980 _s = new Solution12980();
-			System.out.println(_s.solution(6));//2
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-
-		try {
-			Solution12980 _s = new Solution12980();
-			System.out.println(_s.solution(2));//5
+			System.out.println(_s.solution(5000));//5
 		} catch (Exception e){
 			e.printStackTrace();
 		}
 	}
 
 	public int solution(int n) {
-		int ans = 0;
-
-		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-		System.out.println("Hello Java");
-
-		return ans;
+		int result = 0;
+		while( n != 0){
+			if((n % 2 ) == 0){
+				n = n >> 1;
+			} else {
+				n--;
+				result++;
+			}
+		}
+		return result;
 	}
 }
