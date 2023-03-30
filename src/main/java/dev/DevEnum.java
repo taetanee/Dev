486897ab73db3afa594c65bd71e1a4aa8f7ed270
taetanee <xpxksl@gmail.com>
2023-03-30 21:03:22 +0900

    DevEnum 수정

--- src/main/java/dev/DevEnum.java
+++ src/main/java/dev/DevEnum.java
@@ -1,10 +1,13 @@
 package dev;
 
-enum Season {
-	봄, 여름, 가을, 겨울
-}
+
 
 public class DevEnum {
+
+	enum Season {
+		봄, 여름, 가을, 겨울
+	}
+
 	public static void main(String[] args) {
 		Season seson = Season.여름;
 		System.out.println(seson.ordinal());
